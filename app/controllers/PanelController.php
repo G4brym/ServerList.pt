5753126d8e478f024d8e1b1cd4b302aa347cdb35
Gabriel Massadas <g4bryrm98@hotmail.com>
2015-05-18 14:19:36 -0400

    Updates

--- app/controllers/PanelController.php
+++ app/controllers/PanelController.php
@@ -150,8 +150,9 @@ class PanelController extends BaseController {
 
 			if(null !== Input::file('banner')){
 				$filename  = $id->mcs_id . '.' . $image->getClientOriginalExtension();
-				$publicpath = public_path() . '/resources/images/minecraft/banners/' . $filename;
+				$publicpath = public_path() . 'resources/images/minecraft/banners/' . $filename;
 				Image::make($image->getRealPath())->save($publicpath);
+				return Redirect::to(URL::to("/panel/servers"))->With('success', 'Servidor e Banner Atualizado.');
 			}
 
 			return Redirect::to(URL::to("/panel/servers"))->With('success', 'Servidor Atualizado.');
