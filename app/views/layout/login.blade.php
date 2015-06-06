5094a34148a8ca3a0e569f30ec0cb4ee4f07b585
Gabriel Massadas <g4bryrm98@hotmail.com>
2015-06-06 18:28:59 -0400

    Updates

--- app/views/layout/login.blade.php
+++ app/views/layout/login.blade.php
@@ -9,6 +9,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Os Melhores Servidores Tugas Num Unico Sitio">
 	<meta name="author" content="G4brym">
+	<meta name="keywords" content="minecraft, minecraft server list, serverlist, minecraft servers, minecraft server info, minecraft serverlist, minecraft top list, minecraft toplist, best minecraft servers, serverlist.pt, serverlist pt, serverlist portugal, servidores em portugal, servidores de portugal, lista de servidores, lista de servidores de portugal, servidores de minecraft, servidores de csgo, servidores de cs, csgo server list, csgo serverlist, svl, svl pt, svlpt">
+	<meta name="language" content="pt-PT">
+	
+	<meta name="robots" content="index,follow">
+	<meta name="googlebot" content="index,follow">
+	
+	<meta property="og:title" content="{{$title}}">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="{{ URL::to('/resources/images/website/logo-square.png') }}">
+	<meta property="og:site_name" content="Serverlist.pt">
+	<meta property="og:description" content="Os Melhores Servidores Tugas Num Unico Sitio">
+
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:image:src" content="{{ URL::to('/resources/images/website/logo-medium.png') }}">
+	<meta property="twitter:site" content="@serverlistpt">
+	<meta property="twitter:title" content="{{$title}}">
+	<meta property="twitter:description" content="Os Melhores Servidores Tugas Num Unico Sitio">
 
 	<title>{{$title}}</title>
 
